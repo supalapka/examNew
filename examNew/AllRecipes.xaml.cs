2e25a529cd87c2fc632857d81662f178516e1449
supalapka <yurabalint0103@gmail.com>
2020-02-06 15:46:12 +0200

    fix "bug" with dishList.add, add contextmenu demo

--- examNew/AllRecipes.xaml.cs
+++ examNew/AllRecipes.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -42,38 +43,58 @@ namespace examNew
             dish2.KindOfDish = "german";
             dish2.OrderOfDish = DishOrder.secondCource;
 
-            Dishes dish3 = new Dishes();
-            dish2.Name = "milkshake";
-            dish2.TypeOfDish = "dessert";
-            dish2.KindOfDish = "unknown";
-            dish2.OrderOfDish = DishOrder.dessert;
+            Dishes dish4 = new Dishes();
+            dish4.Name = "just salad";
+            dish4.TypeOfDish = "salad";
+            dish4.KindOfDish = "unknown";
+            dish4.OrderOfDish = DishOrder.salad;
 
-            //Dishes dish4 = new Dishes();
-            //dish2.Name = "just salad";
-            //dish2.TypeOfDish = "salad";
-            //dish2.KindOfDish = "unknown";
-            //dish2.OrderOfDish = DishOrder.salad;
+            Dishes dish3 = new Dishes();
+            dish3.Name = "milkshake";
+            dish3.TypeOfDish = "dessert";
+            dish3.KindOfDish = "unknown";
+            dish3.OrderOfDish = DishOrder.dessert;
+
+            Dishes dish5 = new Dishes();
+            dish5.Name = "just salad2";
+            dish5.TypeOfDish = "salad";
+            dish5.KindOfDish = "unknown";
+            dish5.OrderOfDish = DishOrder.salad;
 
             DishesList.Add(dish1);
             DishesList.Add(dish2);
-          // DishesList.Add(dish3);
-            // DishesList.Add(dish3);
-            //  DishesList.Add(dish4);
-            //  DishesList = DishSortByOrder(DishesList);
+            DishesList.Add(dish3);
+            DishesList.Add(dish4);
+            DishesList.Add(dish5);
+           DishesList = DishSortByOrder(DishesList);
             DataContext = this;
         }
-        //public List<Dishes> DishSortByOrder( List<Dishes> dishes)
-        //{
-        //    for (int i = 0; i < dishes.Count - 1; i++)
-        //    {
-        //        if (dishes[i].OrderOfDish < dishes[i + 1].OrderOfDish)
-        //        {
-        //            Dishes tmp = dishes[i];
-        //            dishes[i] = dishes[i + 1];
-        //            dishes[i + 1] = tmp;
-        //        }
-        //    }
-        //    return dishes;
-        //}
+        public List<Dishes> DishSortByOrder(List<Dishes> dishes)
+        {
+            for (int counter = 0; counter < dishes.Count - 1; counter++)
+            {
+                for (int i = 0; i < dishes.Count - 1; i++)
+                {
+                    if (dishes[i].OrderOfDish > dishes[i + 1].OrderOfDish)
+                    {
+                        Dishes tmp = dishes[i];
+                        dishes[i] = dishes[i + 1];
+                        dishes[i + 1] = tmp;
+                    }
+                }
+            }
+            return dishes;
+        }
+
+        private void dishList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ContextMenu contextMenu = new ContextMenu();
+            Button buttonWatch = new Button();
+          //  buttonWatch.BorderThickness = ;
+            buttonWatch.Content = "Watch this recipe";
+            contextMenu.Items.Add(buttonWatch);
+            contextMenu.IsOpen = true;
+            //Timer timer = new Timer();
+        }
     }
 }
